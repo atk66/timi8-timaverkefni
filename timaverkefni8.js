e572140f5d701ae127c7de187a31f424d10de586
atk66 <alexthorir@gmail.com>
2025-10-18 22:46:15 +0000

    100

--- timaverkefni8.js
+++ timaverkefni8.js
@@ -20,10 +20,10 @@ const myString = "Hello, World!";
 let myNumber = myFloatNum = 20.99;
 
 // Búið til breytu sem sameina letterD og myText og hefur gildi "DHello"
-let letterD = myText = "DHello"
+let letterD = myText = "DHello";
 
 // Notið Rökvirkja til að breyta trueBool í false
-let trueBool = false
+let trueBool = false;
 
 //// ---> Samanburðarvirkjar (Comparison operators) <--- ///
 
@@ -31,23 +31,42 @@ let trueBool = false
 console.log(myNumber == "15");
 
 /// Notið strict equal to (===) til að bera saman myNumber við eitthvað til að fá true
+console.log(myNumber === 15);
 
 /// Notið not equal to (!=) til að bera saman myNumber við eitthvað til að fá false
+console.log(myNumber != 15);
 
 /// Notið strict not equal to (!==) til að bera saman myNumber við eitthvað til að fá false
+console.log(myNumber !== 15);
 
 /// Notið greater than (>) og berið 2 breytur saman til að fá true
+console.log(9 > 6);
 
 /// Notið less than (<) og berið 2 breytur saman til að fá false
+console.log(9 < 6);
 
 /// Notið greater or equal to (>=) og berið 2 breytur saman til að fá true
+console.log(9 >= 9);
 
 /// Notið less or equal to (<=) og berið 2 breytur saman til að fá true
+console.log(9 <= 9);
 
 //// ---> Rökvirkjar (Logical operators) <--- ///
 
 /// Notið and (&&) og berið 2 breytur saman til að fá false
+const a = 8;
+const b = -6;
+
+console.log(a > 0 && b > 0);
 
 /// Notið or (||) og berið 2 breytur saman til að fá true
+const a = 8;
+const b = -6;
+
+console.log(a > 0 || b > 0);
 
 /// Notið not (!) og berið 1 breytu saman til að fá false
+const a = 8;
+const b = -6;
+
+console.log(!(a > 0 || b > 0));
